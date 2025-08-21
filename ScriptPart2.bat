e8e52e8453d572a8b20f672079b6d3c50842f8cc
snwaters <snwaters@turner-industries.com>
2025-08-21 07:56:54 -0500

    Update ScriptPart2.bat

--- ScriptPart2.bat
+++ ScriptPart2.bat
@@ -6,7 +6,7 @@
 start /wait msiexec /i %~dp0jcagent-msi-signed.msi /quiet JCINSTALLERARGUMENTS="-k jcc_eyJwdWJsaWNLaWNrc3RhcnRVcmwiOiJodHRwczovL2tpY2tzdGFydC5qdW1wY2xvdWQuY29tIiwicHJpdmF0ZUtpY2tzdGFydFVybCI6Imh0dHBzOi8vcHJpdmF0ZS1raWNrc3RhcnQuanVtcGNsb3VkLmNvbSIsImNvbm5lY3RLZXkiOiIzYzZmZTU3MDA3ZWVmNzNmOTg2NmM4MTcxNTU0Y2E2MWRkYzViNmM0In0g /VERYSILENT /SUPPRESSMSGBOXES"
 del C:\osdcloud\Config\Scripts\Shutdown\create_unattended.ps1
 
-timeout /t 120
+timeout /t 60
 
 cmd /c Dism.exe /online /Enable-Feature /FeatureName:Microsoft-Hyper-V /All /NoRestart
 

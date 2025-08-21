e853adfeeb594c4f8f19ef21b41fd28357897469
snwaters <snwaters@turner-industries.com>
2025-08-21 13:48:52 -0500

    Update ScriptPart2.bat

--- ScriptPart2.bat
+++ ScriptPart2.bat
@@ -4,7 +4,7 @@
         :: Continue with the rest of your operations
 
 start /wait msiexec /i %~dp0jcagent-msi-signed.msi /quiet JCINSTALLERARGUMENTS="-k jcc_eyJwdWJsaWNLaWNrc3RhcnRVcmwiOiJodHRwczovL2tpY2tzdGFydC5qdW1wY2xvdWQuY29tIiwicHJpdmF0ZUtpY2tzdGFydFVybCI6Imh0dHBzOi8vcHJpdmF0ZS1raWNrc3RhcnQuanVtcGNsb3VkLmNvbSIsImNvbm5lY3RLZXkiOiIzYzZmZTU3MDA3ZWVmNzNmOTg2NmM4MTcxNTU0Y2E2MWRkYzViNmM0In0g /VERYSILENT /SUPPRESSMSGBOXES"
-del C:\osdcloud\Config\Scripts\Shutdown\create_unattended.ps1
+del C:\osdcloud\Scripts\Shutdown\create_unattended.ps1
 
 timeout /t 60
 

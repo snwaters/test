e2f23a11c6b28816c1340b566098ce886586819b
snwaters <snwaters@turner-industries.com>
2025-08-25 10:14:45 -0500

    Update ScriptPart2.bat

--- ScriptPart2.bat
+++ ScriptPart2.bat
@@ -35,3 +35,5 @@ start /wait powershell.exe -NoProfile -ExecutionPolicy Bypass -File "C:\osdcloud
 
 start /wait powershell.exe -NoProfile -ExecutionPolicy Bypass -File "C:\osdcloud\Scripts\SetupComplete\Windows_update.ps1"
 
+shutdown /r /f /t 0
+

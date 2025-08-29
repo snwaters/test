dded215bc7a40acf29ff0a42a85acc6d11bf97d7
snwaters <snwaters@turner-industries.com>
2025-08-29 08:39:24 -0500

    Update ScriptPart2.bat

--- ScriptPart2.bat
+++ ScriptPart2.bat
@@ -8,6 +8,8 @@ start /wait powershell.exe -NoProfile -ExecutionPolicy Bypass -File "C:\osdcloud
 start /wait msiexec /i %~dp0jcagent-msi-signed.msi /quiet JCINSTALLERARGUMENTS="-k jcc_eyJwdWJsaWNLaWNrc3RhcnRVcmwiOiJodHRwczovL2tpY2tzdGFydC5qdW1wY2xvdWQuY29tIiwicHJpdmF0ZUtpY2tzdGFydFVybCI6Imh0dHBzOi8vcHJpdmF0ZS1raWNrc3RhcnQuanVtcGNsb3VkLmNvbSIsImNvbm5lY3RLZXkiOiIzYzZmZTU3MDA3ZWVmNzNmOTg2NmM4MTcxNTU0Y2E2MWRkYzViNmM0In0g /VERYSILENT /SUPPRESSMSGBOXES"
 del C:\osdcloud\Scripts\Shutdown\create_unattended.ps1
 
+START /WAIT "SentinalOne" "C:\osdcloud\Scripts\SetupComplete\SentinalOne\install.bat"
+
 rem timeout /t 60
 
 start /wait powershell.exe -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/snwaters/test/refs/heads/main/install.ps1 -OutFile C:\osdcloud\Scripts\SetupComplete\install.ps1; #.\install.ps1"

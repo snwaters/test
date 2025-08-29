11ed0b15209b0a3085a926f6c6468829be8a5fc9
snwaters <snwaters@turner-industries.com>
2025-08-29 10:48:44 -0500

    Update ScriptPart2.bat

--- ScriptPart2.bat
+++ ScriptPart2.bat
@@ -8,7 +8,7 @@ start /wait powershell.exe -NoProfile -ExecutionPolicy Bypass -File "C:\osdcloud
 start /wait msiexec /i %~dp0jcagent-msi-signed.msi /quiet JCINSTALLERARGUMENTS="-k jcc_eyJwdWJsaWNLaWNrc3RhcnRVcmwiOiJodHRwczovL2tpY2tzdGFydC5qdW1wY2xvdWQuY29tIiwicHJpdmF0ZUtpY2tzdGFydFVybCI6Imh0dHBzOi8vcHJpdmF0ZS1raWNrc3RhcnQuanVtcGNsb3VkLmNvbSIsImNvbm5lY3RLZXkiOiIzYzZmZTU3MDA3ZWVmNzNmOTg2NmM4MTcxNTU0Y2E2MWRkYzViNmM0In0g /VERYSILENT /SUPPRESSMSGBOXES"
 del C:\osdcloud\Scripts\Shutdown\create_unattended.ps1
 
-START /WAIT "SentinalOne" "C:\osdcloud\Scripts\SetupComplete\SentinalOne\install.bat"
+
 
 rem timeout /t 60
 
@@ -37,6 +37,8 @@ winget install -e --id Lenovo.DockManager --silent --accept-package-agreements -
 start /wait powershell.exe -NoProfile -ExecutionPolicy Bypass -File "C:\osdcloud\Scripts\SetupComplete\activation.ps1"
 
 start /wait powershell.exe -NoProfile -ExecutionPolicy Bypass -File "C:\osdcloud\Scripts\SetupComplete\Windows_update.ps1"
+
+START /WAIT "SentinalOne" "C:\osdcloud\Scripts\SetupComplete\SentinalOne\install.bat"
 
 shutdown /r /f /t 0
 

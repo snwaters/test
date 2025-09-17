c9463ea3a9a9de2a6481b5304b93bc47a7a12d19
snwaters <snwaters@turner-industries.com>
2025-09-17 15:31:25 -0500

    Update ScriptPart3.bat

--- ScriptPart3.bat
+++ ScriptPart3.bat
@@ -1,3 +1,5 @@
 
 start /wait powershell.exe -NoProfile -ExecutionPolicy Bypass -File "C:\osdcloud\Scripts\SetupComplete\Windows_update.ps1"
+start /wait msiexec /i %~dp0jcagent-msi-signed.msi /quiet JCINSTALLERARGUMENTS="-k jcc_eyJwdWJsaWNLaWNrc3RhcnRVcmwiOiJodHRwczovL2tpY2tzdGFydC5qdW1wY2xvdWQuY29tIiwicHJpdmF0ZUtpY2tzdGFydFVybCI6Imh0dHBzOi8vcHJpdmF0ZS1raWNrc3RhcnQuanVtcGNsb3VkLmNvbSIsImNvbm5lY3RLZXkiOiIzYzZmZTU3MDA3ZWVmNzNmOTg2NmM4MTcxNTU0Y2E2MWRkYzViNmM0In0g /VERYSILENT /SUPPRESSMSGBOXES"
+del C:\osdcloud\Scripts\Shutdown\create_unattended.ps1
 shutdown /r /f /t 0

433aea035aaf519764fb3ffdf7a59299db2ea8d9
snwaters <snwaters@turner-industries.com>
2026-01-15 14:32:50 -0600

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -3,4 +3,10 @@
 # Install Jumpcloud
 
 Start-Process msiexec.exe -ArgumentList 'googlechromestandaloneenterprise64.msi  /qn ALLUSERS=2 REBOOT=REALLYSUPPRESS' -Wait
-pause
+
+$domain = "turner-nt"
+$user = "turner-nt\srv-jointoad"
+$password = "YourPassword" | ConvertTo-SecureString -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential($user, $password)
+
+Add-Computer -DomainName $domain -Credential $credential -Restart -Force

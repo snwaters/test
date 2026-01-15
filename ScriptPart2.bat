b74b30824bc76029b3987b9d1ec1e1b9c1dfeabf
snwaters <snwaters@turner-industries.com>
2026-01-15 16:21:44 -0600

    Update ScriptPart2.bat

--- ScriptPart2.bat
+++ ScriptPart2.bat
@@ -11,7 +11,8 @@ powercfg /x -standby-timeout-ac 0
 
 rem timeout /t 60
 
-start /wait powershell.exe -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/snwaters/test/refs/heads/main/install.ps1 -OutFile C:\osdcloud\Scripts\SetupComplete\install.ps1; .\install.ps1"
+start /wait powershell.exe -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/snwaters/test/refs/heads/main/install.ps1 -OutFile C:\osdcloud\Scripts\SetupComplete\install.ps1
+start /wait powershell.exe -NoProfile -ExecutionPolicy Bypass -File "C:\osdcloud\Scripts\SetupComplete\install.ps1"
 
 cmd /c Dism.exe /online /Enable-Feature /FeatureName:Microsoft-Hyper-V /All /NoRestart
 

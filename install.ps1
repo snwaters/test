ec74086b82029649389e0c7f149998ec23d4809a
snwaters <snwaters@turner-industries.com>
2026-01-15 14:39:51 -0600

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -2,11 +2,15 @@
 
 # Install Jumpcloud
 
-Start-Process msiexec.exe -ArgumentList 'googlechromestandaloneenterprise64.msi  /qn ALLUSERS=2 REBOOT=REALLYSUPPRESS' -Wait
+#Start-Process msiexec.exe -ArgumentList 'googlechromestandaloneenterprise64.msi  /qn ALLUSERS=2 REBOOT=REALLYSUPPRESS' -Wait
 
-$domain = "turner-nt"
-$user = "turner-nt\srv-jointoad"
-$password = "YourPassword" | ConvertTo-SecureString -AsPlainText -Force
-$credential = New-Object System.Management.Automation.PSCredential($user, $password)
+$Domain = "turner-industries.com"
+$User   = "turner-nt\srv-sccmjointoad"
+$Pass   = "cbXHmpRSCT7zd8f7CEKP" | ConvertTo-SecureString -AsPlainText -Force
+$Cred   = New-Object System.Management.Automation.PSCredential($User, $Pass)
 
-Add-Computer -DomainName $domain -Credential $credential -Restart -Force
+# The specific OU where the computer should land
+$TargetOU = "OU=Baton Rouge,OU=All_Computers,DC=turner-industries,DC=com"
+
+# Join the domain with the OUPath parameter
+Add-Computer -DomainName $Domain -Credential $Cred -OUPath $TargetOU -Restart

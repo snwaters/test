28436b0886d1e0b1ee13caaa2e691152535b1692
snwaters <snwaters@turner-industries.com>
2026-02-03 10:27:09 -0600

    Update ScriptPart3.bat

--- ScriptPart3.bat
+++ ScriptPart3.bat
@@ -1,4 +1,4 @@
-
+powercfg /x -standby-timeout-ac 0
 start /wait powershell.exe -NoProfile -ExecutionPolicy Bypass -File "C:\osdcloud\Scripts\SetupComplete\Windows_update.ps1"
 rem start /wait msiexec /i %~dp0jcagent-msi-signed.msi /quiet JCINSTALLERARGUMENTS="-k jcc_eyJwdWJsaWNLaWNrc3RhcnRVcmwiOiJodHRwczovL2tpY2tzdGFydC5qdW1wY2xvdWQuY29tIiwicHJpdmF0ZUtpY2tzdGFydFVybCI6Imh0dHBzOi8vcHJpdmF0ZS1raWNrc3RhcnQuanVtcGNsb3VkLmNvbSIsImNvbm5lY3RLZXkiOiIzYzZmZTU3MDA3ZWVmNzNmOTg2NmM4MTcxNTU0Y2E2MWRkYzViNmM0In0g /VERYSILENT /SUPPRESSMSGBOXES"
 start /wait powershell.exe -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/snwaters/test/refs/heads/main/ScriptPart4.bat -OutFile C:\osdcloud\Scripts\SetupComplete\ScriptPart4.bat

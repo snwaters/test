671fd148186b3d08200be09f3430cf83cc2a558b
snwaters <snwaters@turner-industries.com>
2026-02-12 12:21:54 -0600

    Update add_computer_to_full_JC_managed.ps1

--- add_computer_to_full_JC_managed.ps1
+++ add_computer_to_full_JC_managed.ps1
@@ -7,8 +7,9 @@ Install-Module -Name JumpCloud -Force
 $JCAPIKEY = 'jca_8LVjaQ6Wem5Zk7ody6zn41zsfxT6rZSjF8nA'
 Connect-JCOnline $JCAPIKEY
 
-$deviceName = "c22503"
+$deviceName = $env:COMPUTERNAME
 $device = Get-JCSystem | Where-Object { $_.hostName -eq $deviceName }
 $deviceID = $device.Id
 Write-Host "The Device ID is: $deviceID"
+
 Add-JCSystemGroupMember -GroupName "Full JC Managed" -SystemID $deviceId

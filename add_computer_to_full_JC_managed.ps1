1b957e676e38b16fb0fdb433fb6f0da7670daa58
snwaters <snwaters@turner-industries.com>
2026-02-12 10:11:46 -0600

    Add files via upload

---
+++ add_computer_to_full_JC_managed.ps1
@@ -0,0 +1,14 @@
+﻿# Requires the JumpCloud PowerShell Module: 
+Install-Module -Name JumpCloud -Force
+#Install-Module Microsoft.Graph.DeviceManagement
+
+# 1. Connect to JumpCloud API
+# Replace '<YOUR_API_KEY>' with your actual JumpCloud Admin API Key
+$JCAPIKEY = 'jca_8LVjaQ6Wem5Zk7ody6zn41zsfxT6rZSjF8nA'
+Connect-JCOnline $JCAPIKEY
+
+$deviceName = "c22503"
+$device = Get-JCSystem | Where-Object { $_.hostName -eq $deviceName }
+$deviceID = $device.Id
+Write-Host "The Device ID is: $deviceID"
+Add-JCSystemGroupMember -GroupName "Full JC Managed" -SystemID $deviceId
